202cbcc0d939af8656066a34f2f452964582b9b5
copyrenzhe <275804511@qq.com>
2015-04-13 23:02:26 +0800

    init

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,109 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Dota2Api\Api;
+use Dota2Api\Utils\Db;
+
+class ApiController extends Controller {
+
+	public function __construct()
+	{
+		Api::init(API_KEY, array('localhost', 'root', '', 'dota2_db', ''), false);
+	}
+
+
+	/**
+	 * Get Current numbers of Dota2's players
+	 *
+	 * @return int
+	 */
+	public function GetNumberOfCurrentPlayers()
+	{
+		$url = 'http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?appid=570&format=json';
+		$r = http_curl($url);
+		$result = json_decode($r,true);
+		return $result['response']['player_count'];
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function GetOwnedGames()
+	{
+		$url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=C44A91C0289CDD80307D3C76CC9522F2&steamid=76561198134591399&format=json';
+		$r = http_curl($url);
+		// $result = json_decode($r,true);
+		return $r;
+	}
+
+	public function test()
+	{
+		$matchMapperWeb = new Dota2Api\Mappers\MatchMapperWeb(937739703);
+		$match = $matchMapperWeb->load();
+		$map = new Dota2Api\Utils\Map($match->get('tower_status_radiant'), $match->get('tower_status_dire'), $match->get('barracks_status_radiant'), $match->get('barracks_status_dire'));
+		$canvas = $map->getImage();
+		header('Content-Type: image/jpg');
+		imagejpeg($canvas);
+		imagedestroy($canvas);
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
---
+++ app/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+
+	function http_curl($url)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		// curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+		$r = curl_exec($ch);
+		curl_close($ch);
+		return $r;
+	}
+
+
+?>
