8aba0f8e683fc673c80f5318dda033db4cf5d2ab
copyrenzhe <copyrenzhe@gmail.com>
2015-04-22 17:40:52 +0800

    view/index

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,6 +17,11 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
+	<style>
+		.collapse li a{
+			font-family: '微软雅黑';
+		}
+	</style>
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -38,8 +43,8 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
+						<li><a href="{{ url('/auth/login') }}">登陆</a></li>
+						<li><a href="{{ url('/auth/register') }}">注册</a></li>
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,38 @@
 @extends('app')
+<style type="text/css">
+	.container .search-box{
+		width: 500px;
+		margin: 150px auto;
+	}
+
+	.container .search-box .search{
+		width: 400px;
+		padding: 9px 7px;
+		font: 16px arial;
+		float: left;
+	}
+
+	.container .search-box .index-search{
+		line-height: 40px;
+		background-color: #38f;
+		padding: 0px;
+		border: 0;
+		width: 100px;
+		color: white;
+		font-size: 16px;
+	}
+
+</style>
+
 
 @section('content')
 <div class="container">
-	<div class="content">
-		<input type="text" name="search">
+	<div class="header">
+		<form action="{{ url('/search') }}" method="GET">
+			<div class="search-box">
+				<input type="text" name="search" class="search" placeholder="输入国服昵称或数字ID"><input class="index-search" type="submit" value="搜索">
+			</div>
+		</form>
 	</div>
 </div>
 @endsection
